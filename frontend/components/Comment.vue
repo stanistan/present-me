b4b3c3fced61e1c386a05217bb1739f9342446f8
Stan Rozenraukh <stan@stanistan.com>
2023-04-02 21:28:31 -0400

    we have syntax highlighting for diffs

--- frontend/components/Comment.vue
+++ frontend/components/Comment.vue
@@ -4,21 +4,23 @@
       <div class="text-lg font-bold">File Path: {{ comment.path }}</div>
     </div>
     <div class="flex flex-row max-h-[90vh]">
-      <div class="bg-yellow-100 p-3 flex-1">
+      <div class="bg-yellow-100 p-3 flex-none w-2/5">
         <div v-html="commentBody"></div>
       </div>
-      <div class="bg-yellow-200 p-3 flex-1 overflow-scroll">
-        <code class="language-diff text-sm">
-          {{ comment.diff_hunk }}
-        </code>
+      <div class="bg-yellow-100 p-1 flex-grow overflow-scroll text-sm">
+        <pre style="border:0"><code class="language-diff">{{ comment.diff_hunk }}</code></pre>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
 import showdown from 'showdown';
+
+import Prism from 'prismjs';
+import 'prismjs/components/prism-diff'
+import 'prism-themes/themes/prism-material-oceanic.css'
+
 const converter = new showdown.Converter();
 converter.setFlavor('github');
 
@@ -30,5 +32,10 @@ const commentBody = computed(() => {
   const replaced = props.comment.body.replace(/^\s*\d+\.\s*/, '');
   return converter.makeHtml(replaced);
 });
+
+onMounted(() => {
+  Prism.highlightAll();
+});
+
 
 </script>
