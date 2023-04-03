4471a0ccb7068bd316a048a2a041625137aa39d5
Stan Rozenraukh <stan@stanistan.com>
2023-04-03 11:50:09 -0400

    nicer heading/title

--- frontend/components/Review.vue
+++ frontend/components/Review.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="">
-    <div class="">
-      <div class="text-5xl font-extrabold text-center py-3">
+    <div class="flex flex-row p-3 mx-auto place-content-center">
+      <div class="flex-none text-5xl font-extrabold text-right py-3 max-w-2xl">
         <span class="bg-clip-text text-transparent bg-gradient-to-r underline from-pink-500 to-violet-500">
           {{ model.pr.title }}
         </span>
         (#{{ model.pr.number }})
       </div>
-      <div>{{ model.pr.body }}</div>
-      <div>{{ model.review.title }}</div>
-      <div>{{ model.review.body }}</div>
+      <div class="flex-none max-w-prose prose">
+        <div class="m-3 p-3 bg-gray-50 rounded h-full">
+          <div v-html="prBody"></div>
+          <div v-html="reviewBody"></div>
+        </div>
+      </div>
     </div>
     <div class="gap-3">
       <Comment v-for="(comment, idx) in model.comments" :comment="comment" :idx="idx + 1" />
@@ -18,7 +21,21 @@
 </template>
 
 <script setup lang="ts">
+import showdown from 'showdown';
+
+const converter = new showdown.Converter();
+converter.setFlavor('github');
+
 const props = defineProps({
   model: { type: Object, required: true }
 })
+
+const prBody = computed(() => {
+  return converter.makeHtml(props.model.pr.body);
+})
+
+const reviewBody = computed(() => {
+  return converter.makeHtml(props.model.review.body);
+})
+
 </script>
