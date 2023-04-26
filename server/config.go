7276f7d9a71f212ac6f9ab46933df73ad77f86fd
Stan Rozenraukh <stan@stanistan.com>
2023-04-26 16:20:52 -0400

    chore: extract serve_config from config (#32)

--- server/config.go
+++ server/config.go
@@ -1,67 +1,12 @@
 package presentme
 
 import (
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
-
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	dc "github.com/stanistan/present-me/internal/cache"
 )
-
-type ServeConfig struct {
-	// Port describes the port this server runs on.
-	Port     string `default:"8080" env:"PORT"`
-	Hostname string `default:"localhost" env:"HOSTNAME"`
-
-	// Serve desides if we're running in proxy mode (for development)
-	// or if we are going to be serving the content from the static directory
-	// --- which is what happens when we've built our docker image.
-	Serve        string `required:"" enum:"static,proxy" default:"static"`
-	StaticDir    string `optional:"" default:"./static"`
-	ProxyAddress string `optional:"" default:"http://localhost:3000"`
-}
-
-func (c *ServeConfig) IsProxy() bool {
-	return c.Serve == "proxy"
-}
-
-func (c *ServeConfig) Address() string {
-	return c.Hostname + ":" + c.Port
-}
-
-type spaFS struct {
-	r http.FileSystem
-}
-
-func (fs *spaFS) Open(name string) (http.File, error) {
-	f, err := fs.r.Open(name)
-	if os.IsNotExist(err) {
-		return fs.r.Open("index.html")
-	}
-
-	return f, err
-}
-
-func (c *ServeConfig) WebsiteHandler() (http.Handler, error) {
-	if !c.IsProxy() {
-		fs := &spaFS{http.Dir(c.StaticDir)}
-		server := http.FileServer(fs)
-		return server, nil
-	}
-
-	remote, err := url.Parse(c.ProxyAddress)
-	if err != nil {
-		return nil, errors.Wrap(err, "invalid ProxyAddress")
-	}
-
-	proxy := httputil.NewSingleHostReverseProxy(remote)
-	return proxy, nil
-}
 
 type Config struct {
 	ServeConfig
