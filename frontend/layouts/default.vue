5d5f0bd92f5e3cfd09967e93a792adee4649c9e9
Stan Rozenraukh <stan@stanistan.com>
2023-05-08 22:17:58 -0400

    we get a layout!

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <div class="flex-grow">
+      <slot />
+    </div>
+    <div class="flex-none">
+      <footer class="pt-3 pb-5 text-center text-sm font-mono text-xs">
+        <NuxtLink 
+          href="https://github.com/stanistan/present-me" 
+          class="underline underline-offset-8 font-bold"
+        >
+          <em>present-me</em> by stanistan
+        </NuxtLink> 
+      </footer>
+    </div>
+  </div>
+</template>
