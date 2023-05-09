e9e75f869730d2045f10e26e881d43543ab65dc6
Stan Rozenraukh <stan@stanistan.com>
2023-05-08 22:20:27 -0400

    review page uses TopBar and other cleanup

--- frontend/pages/[org]/[repo]/pull/[pull]/review-[review].vue
+++ frontend/pages/[org]/[repo]/pull/[pull]/review-[review].vue
@@ -1,32 +1,24 @@
 <template>
-  <div class="mx-auto">
-    <div class="bg-gradient-to-b from-gray-800 to-black text-white font-mono text-sm text-center py-2 shadow">
-      {{ $route.params.org }}/{{ $route.params.repo }}/pull/{{ $route.params.pull }}/review-{{ $route.params.review }}
+  <TopBar>
+  {{ $route.params.org }}/{{ $route.params.repo }}/pull/{{ $route.params.pull }}/review-{{ $route.params.review }}
+  </TopBar>
+  <div v-if="pending" class="flex flex-col items-stretch">
+    <div class="animate-pulse mx-auto text-center text-4xl pt-10 font-bold ">
+      <GradientText>Loading...</GradientText>
     </div>
-    <div
-      v-if="pending"
-      class="py-10"
-    >
-      <div class="animate-pulse mx-auto text-center text-4xl font-bold">
-        <GradientText>Loading...</GradientText>
+  </div>
+  <div v-else-if="error" class="mx-auto max-w-3xl py-10">
+    <div class="bg-orange-100 px-2 pb-2">
+      <div class="text-xs text-center underline py-4">
+        {{ error }}
+      </div>
+      <div class="bg-white p-4 text-center border border-orange-200 rounded">
+        <code>{{ error.data }}</code>
       </div>
     </div>
-    <div
-      v-else-if="error"
-      class="mx-auto max-w-3xl py-10"
-    >
-      <div class="bg-orange-100 px-2 pb-2">
-        <div class="text-xs text-center underline py-4">
-          {{ error }}
-        </div>
-        <div class="bg-white p-4 text-center border border-orange-200 rounded">
-          <code>{{ error.data }}</code>
-        </div>
-      </div>
-    </div>
-    <div v-else>
-      <ReviewPage :model="data" />
-    </div>
+  </div>
+  <div class="mx-auto max-w-screen-2xl" v-else>
+    <ReviewPage :model="data" />
   </div>
 </template>
 
