17d658fd0205dbdfc9c1a7cd131464c87bc92970
Stan Rozenraukh <stan@stanistan.com>
2023-05-08 22:20:04 -0400

    adding TopBar component
    This has a link back to the home page

---
+++ frontend/components/TopBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-gradient-to-b from-gray-800 to-black text-white font-mono text-sm py-2 shadow">
+    <div class="flex flex-row">
+      <div class="flex-none pl-3">
+        <NuxtLink href="/" class="font-bold hover:text-pink-300">/</NuxtLink>
+      </div>
+      <div class="flex-grow"></div>
+      <div class="flex-none">
+        <slot />
+      </div>
+      <div class="flex-grow"></div>
+    </div>
+  </div>
+</template>
