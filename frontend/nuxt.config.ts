0af252f2ac8fb35309c4da15f0a40df1b0b4fe21
Stan Rozenraukh <stan@stanistan.com>
2023-05-23 12:28:38 -0400

    feat: favicon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -10,7 +10,11 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: 'present-me'
+      title: 'present-me',
+      link: [
+        { rel: "icon", href: "/favicon.ico", sizes: "any" },
+        { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" }
+      ]
     }
   }
 })
