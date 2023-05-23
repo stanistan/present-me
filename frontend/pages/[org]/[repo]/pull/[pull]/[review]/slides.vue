d73917e70302a917910a38392cf6926408f707df
Stan Rozenraukh <stan@stanistan.com>
2023-05-23 10:30:28 -0400

    support md and slides as well

---
+++ frontend/pages/[org]/[repo]/pull/[pull]/[review]/slides.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-center">
+    redirecting...
+  </div>
+</template>
+<script setup type="ts">
+const { params } = useRoute();
+await navigateTo(`/${params.org}/${params.repo}/pull/${params.pull}/review-${params.review}`);
+</script>
