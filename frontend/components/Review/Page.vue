692289bf3fc4d91b0b7aa9f875080a57078c6eca
Stan Rozenraukh <stan@stanistan.com>
2023-06-13 12:35:46 -0400

    updates WIP ;

---
+++ frontend/components/Review/Page.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="gap-3">
+    <ComponentCard>
+      <template #title>
+        <div class="text-md text-xl font-extrabold">
+          <span>(#{{ model.pr.number }})</span>&nbsp;
+          <GradientText>{{ model.pr.title }}</GradientText>
+        </div>
+      </template>
+      <template #body>
+        <div class="px-4 py-4">
+          <Review-MetadataList :model="model" />
+          <div class="markdown">
+            <MarkdownHTML v-if="body.length">
+              {{ body }}
+            </MarkdownHTML>
+          </div>
+        </div>
+      </template>
+    </ComponentCard>
+    <CommentCard
+      v-for="(comment, idx) in model.comments"
+      :key="idx"
+      :comment="comment"
+      :idx="idx + 1"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  model: { type: Object, required: true }
+});
+
+const body = computed(() => {
+ return  [ props.model.pr.body, props.model.review.body ]
+    .filter(x => x && x.length > 0)
+    .join("\n\n---\n\n");
+});
+</script>
