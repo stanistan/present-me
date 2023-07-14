325a64ef1ecf386fdba22c286213de0a6591ce50
Stan Rozenraukh <stan@stanistan.com>
2023-07-14 16:41:36 -0400

    chore: better test structure for diff generation (#92)

---
+++ server/internal/github/diff/hunk_meta_test.go
@@ -0,0 +1,30 @@
+package diff
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseHunkMeta(t *testing.T) {
+	t.Run("first case", func(t *testing.T) {
+		meta, err := ParseHunkMeta("@@ -230,6 +200,9 @@ if (!defined $initial_reply_to && $prompting) {")
+		require.NoError(t, err)
+		require.Equal(t, HunkMeta{
+			Original: HunkRange{StartingAt: 230, NumLines: 6, IgnorePrefix: "+"},
+			New:      HunkRange{StartingAt: 200, NumLines: 9, IgnorePrefix: "-"},
+		}, meta)
+	})
+	t.Run("succeeds", func(t *testing.T) {
+		meta, err := ParseHunkMeta("@@ -0,6 +200,9 @@ if (!defined $initial_reply_to && $prompting) {")
+		require.NoError(t, err)
+		require.Equal(t, HunkMeta{
+			Original: HunkRange{StartingAt: 0, NumLines: 6, IgnorePrefix: "+"},
+			New:      HunkRange{StartingAt: 200, NumLines: 9, IgnorePrefix: "-"},
+		}, meta)
+	})
+	t.Run("errs", func(t *testing.T) {
+		_, err := ParseHunkMeta("@@ -0,6 +a,9 @@ if (!defined $initial_reply_to && $prompting) {")
+		require.Error(t, err)
+	})
+}
